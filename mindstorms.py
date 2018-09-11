fc0afd0202ab00f0bca2950dd8ff5e6dc235c9f1
Pedro Cecchetti Vaz <pedro.cecchetti@gmail.com>
2018-09-10 22:53:31 -0300

    Adding # NOQA comment to URLS

--- mindstorms.py
+++ mindstorms.py
@@ -8,9 +8,8 @@ avatar = movies.Movie("Avatar", 162,
                       """A paraplegic marine dispatched to the moon Pandora on a
                       Unique mission becomes torn between following his orders
                       and protecting the world he feels is his home.""",
-                      'https://resizing.flixster.com/Yq4pN3qojAZDF6QJFd_yE2Zmr'
-                      '1o=/206x305/v1.bTsxMTE3Njc5MjtqOzE3ODU5OzEyMDA7ODAwOzEy'
-                      'MDA')
+                      'https://resizing.flixster.com/Yq4pN3qojAZDF6QJFd_yE2Zmr1o=/206x305/v1.bTsxMTE3Njc5MjtqOzE3ODU5OzEyMDA7ODAwOzEyMDA'  # NOQA
+                      )
 
 moana = movies.Movie("Moana", 107,
                      "https://www.youtube.com/watch?v=LKFuXETZUsI",
@@ -18,9 +17,8 @@ moana = movies.Movie("Moana", 107,
                      by the Demigod Maui reaches Moana's island, she answers
                      the Ocean's call to seek out the Demigod to set things
                      right.""",
-                     'https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF'
-                     '5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_SY1000_CR0,0,674,'
-                     '1000_AL_.jpg')
+                     'https://m.media-amazon.com/images/M/MV5BMjI4MzU5NTExNF5BMl5BanBnXkFtZTgwNzY1MTEwMDI@._V1_SY1000_CR0,0,674,1000_AL_.jpg'  # NOQA
+                     )
 
 it = movies.Movie("It", 135,
                   "https://www.youtube.com/watch?v=FnCdOQsX5kc",
@@ -29,28 +27,24 @@ it = movies.Movie("It", 135,
                     monster, which disguises itself as a clown
                    and preys on the children of Derry, their small Maine
                    town.""",
-                  'https://m.media-amazon.com/images/M/MV5BZDVkZmI0YzAtNzdjY'
-                  'i00ZjhhLWE1ODEtMWMzMWMzNDA0NmQ4XkEyXkFqcGdeQXVyNzYzODM3Mz'
-                  'g@._V1_SY1000_CR0,0,666,1000_AL_.jpg')
+                  'https://m.media-amazon.com/images/M/MV5BZDVkZmI0YzAtNzdjYi00ZjhhLWE1ODEtMWMzMWMzNDA0NmQ4XkEyXkFqcGdeQXVyNzYzODM3Mzg@._V1_SY1000_CR0,0,666,1000_AL_.jpg'  # NOQA
+                  )
 
 the_conjuring = movies.Movie("The Conjuring", 134,
                              "https://www.youtube.com/watch?v=VFsmuRPClr4",
                              """Ed and Lorraine Warren travel to North London
                              to help a single mother raising 4 children alone
                              in a house plagued by a supernatural spirit.""",
-                             'https://m.media-amazon.com/images/M/MV5BZjU5OWV'
-                             'lN2EtODNlYy00MjhhLWI0MDUtMTA3MmQ5MGMwYTZmXkEyXk'
-                             'FqcGdeQXVyNjE5MTM4MzY@._V1_SY1000_CR0,0,674,100'
-                             '0_AL_.jpg')
+                             'https://m.media-amazon.com/images/M/MV5BZjU5OWVlN2EtODNlYy00MjhhLWI0MDUtMTA3MmQ5MGMwYTZmXkEyXkFqcGdeQXVyNjE5MTM4MzY@._V1_SY1000_CR0,0,674,1000_AL_.jpg'  # NOQA
+                             )
 
 wall_e = movies.Movie("Wall-E", 98,
                       "https://www.youtube.com/watch?v=pATSLKKeKVY",
                       """In the distant future, a small waste-collecting
                       robot inadvertently embarks on a space journey that
                       will ultimately decide the fate of mankind.""",
-                      'https://m.media-amazon.com/images/M/MV5BMjExMTg5OTU0N'
-                      'F5BMl5BanBnXkFtZTcwMjMxMzMzMw@@._V1_SY1000_CR0,0,674,'
-                      '1000_AL_.jpg')
+                      'https://m.media-amazon.com/images/M/MV5BMjExMTg5OTU0NF5BMl5BanBnXkFtZTcwMjMxMzMzMw@@._V1_SY1000_CR0,0,674,1000_AL_.jpg'  # NOQA
+                      )
 
 get_out = movies.Movie("Get Out", 104,
                        "https://www.youtube.com/watch?v=DzfpyUB60YY",
@@ -58,9 +52,8 @@ get_out = movies.Movie("Get Out", 104,
                         parents for the weekend, where his simmering uneasiness
                         about their reception of him eventually reaches a
                         boiling point.""",
-                       'https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyN'
-                       'l5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_SY1000_CR0,0,675'
-                       ',1000_AL_.jpg')
+                       'https://m.media-amazon.com/images/M/MV5BMjUxMDQwNjcyNl5BMl5BanBnXkFtZTgwNzcwMzc0MTI@._V1_SY1000_CR0,0,675,1000_AL_.jpg'  # NOQA
+                       )
 
 # Creating movies Array
 movie_array = [avatar, moana, it, wall_e, get_out, the_conjuring]
